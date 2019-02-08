812cbd95a2c3878c9c3176d8abb69f38dc88e9c2
Tyler Riti <me@tylerriti.org>
2019-02-07 20:38:32 -0600

    added new charcoal recipes and updated graphics

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -13,6 +13,44 @@ data:extend(
     {
         -- general properties
         type = "recipe",
+        name = "wood-charcoal",
+        category = "smelting",
+        -- recipe data
+        ingredients = {{"wood", 4}},
+        result = "coal",
+        energy_required = 3.5,
+    },
+    {
+        type = "recipe",
+        name = "raw-wood-charcoal-adv",
+        order = "b[fluid-chemistry]-b[raw-wood-charcoal]",
+        category = "chemistry",
+        subgroup = "fluid-recipes",
+        icon = "__WoodProcessing__/graphics/icons/raw-wood-charcoal-adv.png",
+        icon_size = 32,
+        crafting_machine_tint =
+        {
+          primary = {r = 0.729, g = 0.604, b = 0.424, a = 0.000},
+          secondary = {r = 0.467, g = 0.400, b = 0.298, a = 0.000},
+          tertiary = {r = 0.408, g = 0.278, b = 0.184, a = 0.000},
+        },
+        ingredients =
+        {
+            {type = "item", name = "raw-wood", amount = 20},
+            {type = "fluid", name = "water", amount = 30},
+        },
+        results =
+        {
+            {type = "fluid", name = "heavy-oil", amount = 1},
+            {type = "item", name = "coal", amount = 10},
+        },
+        energy_required = 10,
+        enabled = false,
+        allow_decomposition = false,
+    },
+    {
+        -- general properties
+        type = "recipe",
         name = "raw-wood-gasification",
         order = "b[fluid-chemistry]-b[raw-wood-gasification]",
         category = "chemistry",
--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -3,9 +3,9 @@ data:extend(
     {
         -- general properties
         type = "technology",
-        name = "wood-gasification",
+        name = "adv-wood-charcoal",
         order = "d-b",
-        icon = "__base__/graphics/technology/oil-processing.png",
+        icon = "__WoodProcessing__/graphics/technology/adv-wood-charcoal.png",
         icon_size = 128,
         -- technology data
         unit =
@@ -19,6 +19,27 @@ data:extend(
             },
         },
         prerequisites = {"oil-processing"},
+        effects = {{type = "unlock-recipe", recipe = "raw-wood-charcoal-adv"}},
+    },
+    {
+        -- general properties
+        type = "technology",
+        name = "wood-gasification",
+        order = "d-b",
+        icon = "__WoodProcessing__/graphics/technology/wood-gasification.png",
+        icon_size = 128,
+        -- technology data
+        unit =
+        {
+            count = 75,
+            time = 30,
+            ingredients =
+            {
+                {"science-pack-1", 1},
+                {"science-pack-2", 1},
+            },
+        },
+        prerequisites = {"adv-wood-charcoal"},
         effects = {{type = "unlock-recipe", recipe = "raw-wood-gasification"}},
     },
 })
